6a73717c129c0bdb4d44518aecddf106b396e071
hellomustaq <hellomstq@gmail.com>
2019-01-30 23:16:49 +0600

    initial

---
+++ master/header.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<!-- Mirrored from html.physcode.com/travel/home-4.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 26 Jan 2019 17:12:10 GMT -->
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="xmlrpc.html">
+	<title>Travel Home Page 4</title>
+	<!-- <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700"> -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css" media="all">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css" type="text/css" media="all">
+	<link rel="stylesheet" href="assets/css/flaticon.css" type="text/css" media="all">
+	<link rel="stylesheet" href="assets/css/font-linearicons.css" type="text/css" media="all">
+	<link rel="stylesheet" href="style.css" type="text/css" media="all">
+	<link rel="stylesheet" href="assets/css/travel-setting-home-4.css" type="text/css" media="all">
+	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+</head>
+<body>
+<div class="wrapper-container">
+	<header id="masthead" class="site-header sticky_header affix-top">
+		<div class="header_top_bar">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<aside id="text-15" class="widget_text">
+							<div class="textwidget">
+								<ul class="top_bar_info clearfix">
+									<li><i class="fa fa-info"></i> A tour platform to find everything!</li>
+								</ul>
+							</div>
+						</aside>
+					</div>
+					<div class="col-sm-8 topbar-right">
+						<aside id="text-7" class="widget widget_text">
+							<div class="textwidget">
+								<ul class="top_bar_info clearfix">
+									<li><i class="fa fa-phone"></i> 0123456789</li>
+									<li class="hidden-info">
+										<i class="fa fa-map-marker"></i> Dhaka Bangladesh
+									</li>
+								</ul>
+							</div>
+						</aside>
+						<aside id="travel_login_register_from-2" class="widget widget_login_form">
+							<span class="show_from login"><i class="fa fa-user"></i>Login</span>
+
+							<div class="form_popup from_login" tabindex="-1">
+								<div class="inner-form">
+									<div class="closeicon"></div>
+									<h3>Login</h3>
+									<form name="loginform" id="loginform" action="#" method="post">
+										<p class="login-username">
+											<label for="user_login">Username or Email Address</label>
+											<input type="text" name="log" id="user_login" class="input" value="" size="20">
+										</p>
+										<p class="login-password">
+											<label for="user_pass">Password</label>
+											<input type="password" name="pwd" id="user_pass" class="input" value="" size="20">
+										</p>
+										<p class="login-remember">
+											<label><input name="rememberme" type="checkbox" id="rememberme" value="forever"> Remember Me</label>
+										</p>
+										<p class="login-submit">
+											<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary" value="Log In">
+											<input type="hidden" name="redirect_to" value="">
+										</p>
+									</form>
+									<a href="#" title="Lost your password?" class="lost-pass">Lost your password?</a>
+								</div>
+							</div>
+							<span class="register_btn">Register</span>
+							<div class="form_popup from_register" tabindex="-1">
+								<div class="inner-form">
+									<div class="closeicon"></div>
+									<h3>Register</h3>
+									<form method="post" class="register">
+										<p class="form-row">
+											<label for="reg_username">Username <span class="required">*</span></label>
+											<input type="text" class="input" name="username" id="reg_username" value="" autocomplete="off">
+										</p>
+										<p class="form-row">
+											<label for="reg_email">Email address <span class="required">*</span></label>
+											<input type="email" class="input" name="email" id="reg_email" value="">
+										</p>
+										<p class="form-row">
+											<label for="reg_password">Password <span class="required">*</span></label>
+											<input type="password" class="input" name="password" id="reg_password">
+										</p>
+										<div style="left: -999em; position: absolute;">
+											<label for="trap">Anti-spam</label><input type="text" name="email_2" id="trap" tabindex="-1" autocomplete="off">
+										</div>
+										<p class="form-row">
+											<input type="submit" class="button" name="register" value="Register">
+										</p>
+									</form>
+								</div>
+							</div>
+							<div class="background-overlay"></div>
+						</aside>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="navigation-menu">
+			<div class="container">
+				<div class="menu-mobile-effect navbar-toggle button-collapse" data-activates="mobile-demo">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</div>
+				<div class="width-logo sm-logo">
+					<a href="#" title="Travel" rel="home">
+						<img src="images/logo.png" alt="Logo" width="474" height="130" class="logo_transparent_static">
+						<img src="images/logo_sticky.png" alt="Sticky logo" width="474" height="130" class="logo_sticky">
+					</a>
+				</div>
+				<nav class="width-navigation">
+					<ul class="nav navbar-nav menu-main-menu side-nav" id="mobile-demo">
+						<li class="">
+							<a href="index-2.html">Home</a>
+						</li>
+						<li class="">
+							<a href="tours.html">Tours</a>
+						</li>
+
+						<li><a href="destinations.html">Destinations</a></li>
+						<li><a href="blog.html">Blog</a></li>
+
+						<li class="">
+							<a href="#">Pages</a>
+						</li>
+						<li><a href="contact.html">Contact</a></li>
+						<li class="menu-right">
+							<ul>
+								<li id="travel_social_widget-2" class="widget travel_search">
+									<div class="search-toggler-unit">
+										<div class="search-toggler">
+											<i class="fa fa-search"></i>
+										</div>
+									</div>
+									<div class="search-menu search-overlay search-hidden">
+										<div class="closeicon"></div>
+										<form method="get" class="search-form" action="http://html.physcode.com/travel/tours.html">
+											<input type="search" class="search-field" placeholder="Search ..." value="" name="s" title="Search for:">
+											<input type="submit" class="search-submit font-awesome" value="&#xf002;">
+										</form>
+										<div class="background-overlay"></div>
+									</div>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+
+
