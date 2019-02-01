c1c1d4e2c398d68aae16b71711fec2e7d6f4dd11
Nur Alam <furiousnur@gmail.com>
2019-02-01 17:01:33 +0600

    add blog details page

---
+++ blog_details.php
@@ -0,0 +1,31 @@
+<?php include('views/master/header.php'); ?>
+<div class="site wrapper-content">
+	<div class="top_site_main">
+	</div>
+	<section class="content-area">
+		<div class="container">
+			<div class="row">
+				<header class="entry-header">
+					<h1 class="entry-title">World’s hottest destinations for vegans</h1>
+					<div class="entry-meta">
+						<span class="posted-on">Posted on <a href="single.html" rel="bookmark">
+							<time class="entry-date published" datetime="2016-09-06T04:52:25+00:00">September 6, 2016</time>
+						</a></span>
+					</div>
+				</header>
+				<div class="img_post"><a href="single.html">
+				<img style="width: 100%; height: 550px; margin-top: 10px;  border-radius: 10px;" src="images/blog/86H.jpg" class="wp-post-image" alt=""></a>
+			</div>
+			<div class="site-main col-sm-12">
+				<div class="wrapper-blog-content"><div class="entry-content content-thumbnail">
+							<div class="entry-desc">
+								<p style=" margin-top: 10px; font-size: 18px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam efficitur egestas risus. Sed eros augue, tempor et faucibus eu, cursus ac lacus. Ut sodales semper ante, at malesuada neque vestibulum Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam efficitur egestas risus. Sed eros augue, tempor et faucibus eu, cursus ac lacus. Ut sodales semper ante, at malesuada neque vestibulum </p>
+							</div>
+						</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+</div>
+<?php include('views/master/footer.php'); ?>
--- views/master/header.php
+++ views/master/header.php
@@ -12,6 +12,8 @@
 	<link rel="stylesheet" href="assets/css/font-awesome.min.css" type="text/css" media="all">
 	<link rel="stylesheet" href="assets/css/flaticon.css" type="text/css" media="all">
 	<link rel="stylesheet" href="assets/css/font-linearicons.css" type="text/css" media="all">
+	<link rel="stylesheet" href="assets/css/booking.css" type="text/css" media="all">
+	<link rel="stylesheet" href="assets/css/swipebox.min.css" type="text/css" media="all">
 	<link rel="stylesheet" href="style.css" type="text/css" media="all">
 	<link rel="stylesheet" href="assets/css/travel-setting-home-4.css" type="text/css" media="all">
 	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
