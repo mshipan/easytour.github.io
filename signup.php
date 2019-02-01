70f35acc251ec7bdbb737ad01cfb91819cb611e9
hellomustaq <hellomstq@gmail.com>
2019-02-01 17:17:40 +0600

    registration dynamic

--- signup.php
+++ signup.php
@@ -1,8 +1,40 @@
+<?php
+    session_start();
+    require'vendor/autoload.php';
+    include 'function.php';
+    $rfirstname=$rlastname=$rusername=$remail=$rcountry=$rgender=$rpassword=$rerror=$duplicateuser=$duplicateemail="";
+if (isset($_POST['registration'])) {
+    if ($_SERVER["REQUEST_METHOD"]=="POST") {
+    $name=validator($_POST['name']);
+    $email=validator($_POST['email']);
+    $phone=validatorPass($_POST['phone']);
+    $village=validator($_POST['village']);
+    $upozila=validator($_POST['upozila']);
+    $district=validator($_POST['district']);
+    $country=validator($_POST['country']);
+    $zip=validator($_POST['zip']);
+    $password=md5(validatorPass($_POST['password']));
+
+    $user=User::insert([
+        'name' => $name,
+        'email' => $email,
+        'phone' => $phone,
+        'village' => $village,
+        'upozila' => $upozila,
+        'district' => $district,
+        'country' => $country,
+        'zip_code' => $zip,
+        'password' => $password,
+    ]);
+    }
+}
+
+ ?>
 <?php include('views/master/header.php'); ?>
 
 <!-- always need start -->
 <div class="site wrapper-content">
-    <br><br><br>
+    <br>
     <section class="content-area">
         <div class="container">
             <div class="row">
@@ -14,72 +46,89 @@
 
 
                             <div class="travel_tour-info-login-form">
-                                <h2 style="text-align: center;">Register as a new user..</h2><br>
+                                <h2 style="text-align: center;">Register as a new user...</h2><br>
                                 <div class="row justify-content-center">
-                                    <div class="col-md-3"></div>
-                                    <div class="col-md-8">
+                                    <div class="col-md-2"></div>
+                                    <div class="col-md-9">
                                         <div class="card">
                                             <div class="card-body">
-                                                <form name="my-form" onsubmit="return validform()" action="success.php" method="">
+                                                <form  action="" method="post">
                                                     <div class="form-group row">
-                                                        <label for="full_name" class="col-md-4 col-form-label text-md-right">Full Name</label>
-                                                        <div class="col-md-6">
-                                                            <input type="text" id="full_name" class="form-control" name="full-name">
+                                                        <label for="full_name" class="col-md-2 col-form-label text-md-right">Full Name </label>
+                                                        <div class="col-md-8">
+                                                            <input type="text" id="full_name" class="form-control" name="name">
                                                         </div>
                                                     </div>
                                                     <div class="form-group row">
-                                                        <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                                                        <div class="col-md-6">
-                                                            <input type="text" id="email_address" class="form-control" name="email-address">
+                                                        <label for="email_address" class="col-md-2 col-form-label text-md-right">E-Mail Address</label>
+                                                        <div class="col-md-8">
+                                                            <input type="email" id="email_address" class="form-control" name="email">
                                                         </div>
                                                     </div>
                                                     <div class="form-group row">
-                                                        <label for="user_name" class="col-md-4 col-form-label text-md-right">User Name</label>
-                                                        <div class="col-md-6">
-                                                            <input type="text" id="user_name" class="form-control" name="username">
+                                                        <label for="phone_number" class="col-md-2 col-form-label text-md-right">Phone Number</label>
+                                                        <div class="col-md-8">
+                                                            <input type="text" name="phone" id="phone_number" class="form-control">
                                                         </div>
                                                     </div>
                                                     <div class="form-group row">
-                                                        <label for="phone_number" class="col-md-4 col-form-label text-md-right">Phone Number</label>
-                                                        <div class="col-md-6">
-                                                            <input type="text" id="phone_number" class="form-control">
+                                                        <label for="user_name" class="col-md-2 col-form-label text-md-right">Password</label>
+                                                        <div class="col-md-3">
+                                                            <input type="password" id="user_name" class="form-control" name="password">
+                                                        </div>
+                                                        <label style="text-align: right;" for="user_name" class="col-md-2 col-form-label text-md-right">Confirm Password</label>
+                                                        <div class="col-md-3">
+                                                            <input type="password" id="user_name" class="form-control" name="password">
+                                                        </div>
+                                                    </div>
+                                                    
+                                                    <div class="form-group row">
+                                                        <label for="present_address" class="col-md-2 col-form-label text-md-right">Village</label>
+                                                        <div class="col-md-3">
+                                                            <input type="text" name="village" id="present_address" class="form-control">
+                                                        </div>
+                                                        <label style="text-align: right;" for="permanent_address" class="col-md-2 col-form-label text-md-right">Upozila</label>
+                                                        <div class="col-md-3">
+                                                            <input type="text" name="upozila" id="permanent_address" class="form-control" name="permanent-address">
                                                         </div>
                                                     </div>
                                                     <div class="form-group row">
-                                                        <label for="present_address" class="col-md-4 col-form-label text-md-right">Present Address</label>
-                                                        <div class="col-md-6">
-                                                            <input type="text" id="present_address" class="form-control">
+                                                        <label for="permanent_address" class="col-md-2 col-form-label text-md-right">District</label>
+                                                        <div class="col-md-3">
+                                                            <input type="text" name="district" id="permanent_address" class="form-control" name="permanent-address">
+                                                        </div>
+                                                        <label style="text-align: right;" for="permanent_address" class="col-md-2 col-form-label text-md-right">Country</label>
+                                                        <div class="col-md-3">
+                                                            <input type="text" name="country" id="permanent_address" class="form-control" name="permanent-address">
                                                         </div>
                                                     </div>
                                                     <div class="form-group row">
-                                                        <label for="permanent_address" class="col-md-4 col-form-label text-md-right">Permanent Address</label>
-                                                        <div class="col-md-6">
-                                                            <input type="text" id="permanent_address" class="form-control" name="permanent-address">
+                                                        <label for="permanent_address" class="col-md-2 col-form-label text-md-right">Zip Code</label>
+                                                        <div class="col-md-8">
+                                                            <input type="text" name="zip" id="permanent_address" class="form-control" >
                                                         </div>
                                                     </div>
                                                     <div class="form-group row">
-                                                        <label for="nid_number" class="col-md-4 col-form-label text-md-right"><abbr
-                                                        title="National Id Card">NID</abbr> Number</label>
-                                                        <div class="col-md-6">
-                                                            <input type="text" id="nid_number" class="form-control" name="nid-number">
+                                                        <label for="permanent_address" class="col-md-2 col-form-label text-md-right">Profile Image <small style="color: red;">300*200 | under 1MB</small></label>
+                                                        <div class="col-md-8">
+                                                            <input type="file" name="image" id="permanent_address" class="form-control" >
                                                         </div>
                                                     </div>
                                                     <div class="form-group row">
-                                                        <label for="nid_number" class="col-md-4 col-form-label text-md-right"></label>
-                                                        <div class="col-md-6">
-                                                            <button type="submit" class="btn btn-primary">
+                                                        <label for="nid_number" class="col-md-2 col-form-label text-md-right"></label>
+                                                        <div class="col-md-8">
+                                                            <button name="registration" type="submit" class="btn btn-primary">
                                                         Register
                                                         </button>
                                                         </div>
                                                     </div>
-                                                </div>
-                                            </form>
+                                                </form>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-2"></div>
+                                    <div class="col-md-1"></div>
                                 </div>
                             </div>
-                        </div>
 
 
 
