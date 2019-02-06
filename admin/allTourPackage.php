2afbde4112eec62c1a44ee5af8dd9c010b589b69
Nur Alam <furiousnur@gmail.com>
2019-02-06 15:24:23 +0600

    data show for all tour page & all hotel package page

---
+++ admin/allTourPackage.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+if ($_SESSION['user']!=1) {
+	header('location:login.php');
+}
+require 'master/header.php';
+require'../vendor/autoload.php';
+$tours=TourPackage::all();
+?>
+<table class="table">
+	<thead class="thead-dark">
+		<tr>
+			<th scope="col">#</th>
+			<th scope="col">Tour Name</th>
+			<th scope="col">City</th>
+			<th scope="col">Country</th>
+			<th scope="col">Amount</th>
+			<th scope="col">Edit Action</th>
+			<th scope="col">Delete Action</th>
+		</tr>
+	</thead>
+	<?php foreach ($tours as $key => $tour) { ?>
+	<tbody>
+		<tr>
+			<th scope="row">1</th>
+			<td> <?php echo $tour->tour_name; ?></td>
+			<td> <?php echo $tour->city; ?></td>
+			<td> <?php echo $tour->country; ?></td>
+			<td> <?php echo $tour->amount; ?></td>
+			<td> Edit</td>
+			<td> Delete</td>
+		</tr>
+	</tbody>
+	<?php } ?>
+</table>
+<?php
+	require 'master/footer.php';
+?>
